666ca77492192460d4c67b522641ade9cf99a262
𓆩Jordan 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-09-15 17:48:44 +0500

    Update ig.py

--- ig.py
+++ ig.py
@@ -2,7 +2,7 @@ import os,re,time,sys,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from ig import XYZ
+    from YOUNIS import XYZ
     XYZ()
 elif bit == '32bit':
     print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
