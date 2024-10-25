e14e98d596b5c52552fc49e791f9cd143bed2044
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-25 09:27:12 +0500

    Update ig.py

--- ig.py
+++ ig.py
@@ -1,10 +1,26 @@
-import os,re,time,sys,platform
-os.system('git pull')
+#coding=utf-8
+# Github : https://github.com/younis-dgk
+# Whatsapp : +923194999455
+# More About : https://bio.link/younis_dgk
+import os, sys, platform
+print('\033[0;97m [💸]\033[92m join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
+os.system('clear')
+print('\033[0;97m [💚] \033[92mChecking For Updates...')
+os.system('git pull --quiet 2>/dev/null')
+os.system('rm -rf ig.so')
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf ig.so')
+except:
+    pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from YOUNIS import XYZ
-    XYZ()
+    if not os.path.isfile('ig.so'):
+        os.system('curl -L https://github.com/younis-dgk/CrackIG/blob/main/ig.cpython-312.so?raw=true -o ig.so') 
+        import ig
+    else:
+        import ig
 elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
- 
+    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔 \033[1;90m]");exit()
  
